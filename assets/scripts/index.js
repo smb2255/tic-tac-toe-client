da7e3ae3866f52ad444a5ab7d4d1e8c977f533dd
smb2255 <smb2255@columbia.edu>
2017-12-22 12:28:47 -0500

    Got the board to console log each square's ID
    - next step is to write a function so that an 'x' can show up on each box once you click on it.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,13 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-
+const events = require('./events')
 $(() => {
   setAPIOrigin(location, config)
+})
+
+$(() => {
+  events.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
